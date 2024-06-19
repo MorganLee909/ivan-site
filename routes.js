2c4529833939d2d1675539d6c9856bf9a06f504d
Lee Morgan <lee@leemorgan.dev>
2024-06-19 22:20:35 +0300

    Complete insight cat section.

--- routes.js
+++ routes.js
@@ -2,4 +2,12 @@ module.exports = (app)=>{
     app.get("/", (req, res)=>{
         res.sendFile(`${__dirname}/views/landing.html`);
     });
+
+    app.get("/landing.css", (req, res)=>{
+        res.sendFile(`${__dirname}/views/landing.css`);
+    })
+
+    app.get("/image/:file", (req, res)=>{
+        res.sendFile(`${__dirname}/views/images/${req.params.file}`);
+    });
 }
