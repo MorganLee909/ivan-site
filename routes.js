1a4e7f278adf0c87d9ad7be760f6b6728252916c
Lee Morgan <lee@leemorgan.dev>
2024-06-20 19:42:01 +0300

    wip

--- routes.js
+++ routes.js
@@ -5,6 +5,14 @@ module.exports = (app)=>{
 
     app.get("/landing.css", (req, res)=>{
         res.sendFile(`${__dirname}/views/landing.css`);
+    });
+
+    app.get("/insight-cat", (req, res)=>{
+        res.sendFile(`${__dirname}/views/insightCat.html`);
+    });
+
+    app.get("/insight-cat.css", (req, res)=>{
+        res.sendFile(`${__dirname}/views/insightCat.css`);
     })
 
     app.get("/image/:file", (req, res)=>{
